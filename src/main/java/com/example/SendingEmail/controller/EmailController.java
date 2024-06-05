effa73044b4fff8fd63ac6fa554ac1e3b12ae6e7
atchaya1617 <atchayaravi04@gmail.com>
2024-06-05 13:00:56 +0530

    issue fix

--- src/main/java/com/example/SendingEmail/controller/EmailController.java
+++ src/main/java/com/example/SendingEmail/controller/EmailController.java
@@ -5,7 +5,9 @@ import com.example.SendingEmail.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class EmailController {
 
     @Autowired
--- src/main/java/com/example/SendingEmail/service/EmailSendingImpl.java
+++ src/main/java/com/example/SendingEmail/service/EmailSendingImpl.java
@@ -9,9 +9,10 @@ import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
 
 import java.io.File;
-
+@Service
 public class EmailSendingImpl implements EmailService {
 
     @Autowired
